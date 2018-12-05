2fdcc8a0b0b0f39c8435b87ca758ffc0c7b9a02e
patrickjdeng <patrickjdeng@gmail.com>
2018-12-05 10:35:24 -0600

    added tester for accuracy against gold

---
+++ testacc.py
@@ -0,0 +1,23 @@
+import sys
+
+def read_file(filename):
+    infile = open(filename)
+    array = []
+    for line in infile:
+        array.append(line.strip('\n'))
+    print(array)
+    print('\n\n\n\n\n')
+    return array
+
+def main():
+    predictions_array = read_file(sys.argv[1])
+    actual_array = read_file(sys.argv[2])
+    correct = 0
+    total = len(predictions_array)
+    for i in range(len(predictions_array)):
+        if predictions_array[i] == actual_array[i]:
+            correct += 1
+    percentage = float(correct)/float(total)*100
+    print("Total Accuracy is: " + str(percentage))
+    
+main()
